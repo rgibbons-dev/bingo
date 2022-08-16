6a1b20172292c50313f39c832e7719c01d9ec8f3
rgibbons-dev <61358729+rgibbons-dev@users.noreply.github.com>
2022-08-16 13:54:31 -0400

    prototype

--- script.js
+++ script.js
@@ -22,4 +22,13 @@ function gen() {
     let ocol = col(61,75);
 }
 
-document.body.innerHTML = "<h1>test item: " + bcol[1] + "</h1>" 
+gen();
+
+let header = "<div class=\"cell\">b</div> <div class=\"cell\">i</div> <div class=\"cell\">n</div> <div class=\"cell\">g</div> <div class=\"cell\">o</div>";
+let col0 = "<div class=\"cell\">" + bcol[0] + "</div> <div class=\"cell\">" + icol[0] + "</div> <div class=\"cell\">" + ncol[0] + "</div> <div class=\"cell\">" + gcol[0] + "</div> <div class=\"cell\">" + ocol[0] + "</div>";
+let col1 = "<div class=\"cell\">" + bcol[1] + "</div> <div class=\"cell\">" + icol[1] + "</div> <div class=\"cell\">" + ncol[1] + "</div> <div class=\"cell\">" + gcol[1] + "</div> <div class=\"cell\">" + ocol[1] + "</div>";
+let col2 = "<div class=\"cell\">" + bcol[2] + "</div> <div class=\"cell\">" + icol[2] + "</div> <div class=\"cell\">" + ncol[2] + "</div> <div class=\"cell\">" + gcol[2] + "</div> <div class=\"cell\">" + ocol[2] + "</div>";
+let col3 = "<div class=\"cell\">" + bcol[3] + "</div> <div class=\"cell\">" + icol[3] + "</div> <div class=\"cell\">" + ncol[3] + "</div> <div class=\"cell\">" + gcol[3] + "</div> <div class=\"cell\">" + ocol[3] + "</div>";
+let col4 = "<div class=\"cell\">" + bcol[4] + "</div> <div class=\"cell\">" + icol[4] + "</div> <div class=\"cell\">" + ncol[4] + "</div> <div class=\"cell\">" + gcol[4] + "</div> <div class=\"cell\">" + ocol[4] + "</div>";
+
+document.body.innerHTML = "<div id=\"grid-col\">" + header + col0 + col1 + col2 + col3 + col4 + "</div>"
