7c7ec702eabe17f8cf1101a824843a527ee38748
rgibbons-dev <61358729+rgibbons-dev@users.noreply.github.com>
2022-08-16 21:24:46 -0400

    almost forgot the free space

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function gen() {
     let header = "<div class=\"cell\">b</div> <div class=\"cell\">i</div> <div class=\"cell\">n</div> <div class=\"cell\">g</div> <div class=\"cell\">o</div>";
     let col0 = "<div class=\"cell\">" + bcol[0] + "</div> <div class=\"cell\">" + icol[0] + "</div> <div class=\"cell\">" + ncol[0] + "</div> <div class=\"cell\">" + gcol[0] + "</div> <div class=\"cell\">" + ocol[0] + "</div>";
     let col1 = "<div class=\"cell\">" + bcol[1] + "</div> <div class=\"cell\">" + icol[1] + "</div> <div class=\"cell\">" + ncol[1] + "</div> <div class=\"cell\">" + gcol[1] + "</div> <div class=\"cell\">" + ocol[1] + "</div>";
-    let col2 = "<div class=\"cell\">" + bcol[2] + "</div> <div class=\"cell\">" + icol[2] + "</div> <div class=\"cell\">" + ncol[2] + "</div> <div class=\"cell\">" + gcol[2] + "</div> <div class=\"cell\">" + ocol[2] + "</div>";
+    let col2 = "<div class=\"cell\">" + bcol[2] + "</div> <div class=\"cell\">" + icol[2] + "</div> <div class=\"cell\">" + "FREE" + "</div> <div class=\"cell\">" + gcol[2] + "</div> <div class=\"cell\">" + ocol[2] + "</div>";
     let col3 = "<div class=\"cell\">" + bcol[3] + "</div> <div class=\"cell\">" + icol[3] + "</div> <div class=\"cell\">" + ncol[3] + "</div> <div class=\"cell\">" + gcol[3] + "</div> <div class=\"cell\">" + ocol[3] + "</div>";
     let col4 = "<div class=\"cell\">" + bcol[4] + "</div> <div class=\"cell\">" + icol[4] + "</div> <div class=\"cell\">" + ncol[4] + "</div> <div class=\"cell\">" + gcol[4] + "</div> <div class=\"cell\">" + ocol[4] + "</div>";
 
